56a74a244f2b1568e0288d6f0c76ed317e9e7a11
TiagoPdaS <tiagopdasgithub@gmail.com>
2021-02-15 19:26:33 -0500

    Create arrays.php
    criando tipos de arrays

---
+++ arrays.php
@@ -0,0 +1,17 @@
+<?php
+/**Arrays = variaveis com diversas chaves! */
+
+$informacao['nome'] = 'Tiago';
+$informacao['idade'] = 30;
+$informacao['cidade'] = 'Boston';
+
+echo $informacao['nome'];
+echo '<br/>';
+echo $informacao['idade'];
+echo '<br/>';
+echo $informacao['cidade'];
+echo '<br/>';
+
+
+
+?>
