9c82386a0bbb39037e559ca5889cb8c4e084cda4
TiagoPdaS <tiagopdasgithub@gmail.com>
2021-02-15 20:21:20 -0500

    Create concatencao.php
    concatenacao

---
+++ concatencao.php
@@ -0,0 +1,16 @@
+<?php
+$frase = 'My name is ';
+$nome = 'Tiago';
+$idade = 30;
+$nomedaclasse = 'box';
+
+define('NOME','John');
+
+//echo $frase.$nome.$idade;
+
+//echo 'helo, my name is '.$nome;
+
+//echo "hello, my name is $nome";
+
+echo "<div class=\"nomedaclasse\">My text</div>";
+?>
