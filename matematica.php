cf9bc021ff881775f5f45b3c839ed25bccee2c9c
TiagoPdaS <tiagopdasgithub@gmail.com>
2021-02-15 20:32:00 -0500

    Create matematica.php
    fazendo algumas operacoes matematicas..

---
+++ matematica.php
@@ -0,0 +1,17 @@
+<?php
+$numero = 5+5;
+
+$numero2 = 10-5;
+
+$resultado = $numero+$numero2;
+$resultado2 = 10/5;
+
+//echo $resultado2;
+
+//expressao 
+//expressao = 20 * (2 + 10);
+
+$resto = 3 % 3;
+
+echo $resto;
+?>
