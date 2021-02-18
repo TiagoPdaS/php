921eb22c3466a070fbeda4c4059de94979a6198c
TiagoPdaS <tiagopdasgithub@gmail.com>
2021-02-17 20:05:33 -0500

    Create loopings.php
    3 exemplos de loopings

---
+++ loopings.php
@@ -0,0 +1,27 @@
+<?php
+
+//laco de repeticao for
+/*for($contador=10;$contador>=0;$contador--){
+    echo 'ola mundo!!!!';
+    echo '<br/>';
+    echo contador;
+    echo '<hr>';
+}
+*/
+
+//laco de repeticao while
+/*$contador = 0;
+while($contador < 10){
+    echo 'ola mundo!!';
+    echo '<hr>';
+    $contador+=1;
+}
+*/
+
+$contador = 0;
+do{
+    echo 'ola mundo!!';
+    echo '<br/>'
+    $contador++;
+}while($contador < 10)
+?>
