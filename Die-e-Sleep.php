f4efc75ae2130d066b34c480dcd49a308b469700
TiagoPdaS <tiagopdasgithub@gmail.com>
2021-02-19 21:08:14 -0500

    Create Die-e-Sleep.php

---
+++ Die-e-Sleep.php
@@ -0,0 +1,31 @@
+<?php
+
+    //durma o codigo por x segundos
+    /*sleep(3);
+    echo'um';
+
+    sleep(3);
+    echo 'dois';
+
+    sleep(3);
+    echo 'tres';
+    */
+
+    $nome = 'john';
+    if($nome == 'joun'){
+        //continuamos o codigo
+        echo 'tudo certo';
+    }else{
+        die("o script parou de ser executado");
+    }
+
+    sleep(3);
+    echo'um';
+
+    sleep(3);
+    echo 'dois';
+
+    sleep(3);
+    echo 'tres';
+
+?>
